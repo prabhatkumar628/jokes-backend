e8f9ed457c03ab0b237451c861ade64eaa3ceb34
Prabha Kumar <kprabhat628@gmail.com>
2024-10-09 16:22:57 +0530

    joke api

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv"
+import fs from "fs"
+dotenv.config({
+    path: "./env"
+})
+
+let data
+const readData = async () => {
+    try {
+        data = await fs.promises.readFile("./src/data.json", "utf-8")
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+readData()
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("<h1>Server is Running with api data</h1>")
+})
+app.get("/api/data", (req, res) => {
+    res.send(JSON.parse(data))
+})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
